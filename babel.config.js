58b4409d115d8fb4f21d0afb066a290ac8d0d8d4
Siamak <ssiasoft@gmail.com>
2023-04-29 10:19:46 +0800

    fix: remove qr scanner

--- babel.config.js
+++ babel.config.js
@@ -2,10 +2,5 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-      'react-native-reanimated/plugin',
-      {
-        globals: ['__scanCodes'],
-      },
-    
   };
 };
